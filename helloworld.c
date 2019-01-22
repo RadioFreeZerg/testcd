a47ca24bf8884daafa3b28bc1f551dd742e36d0b
hperezf <hectorperezfuentes@gmail.com>
2019-01-21 23:59:14 -0300

    Now we have a third line

--- helloworld.c
+++ helloworld.c
@@ -4,5 +4,6 @@ int main()
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
    printf("Bye bye, blackbird.\n");
+   printf("Maybe we need to compile this...\n");
    return 0;
 }
